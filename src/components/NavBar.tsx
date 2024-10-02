75a4a0783fc233047e099e89ff103579f8fc9e20
FabioFlo <fabio.floris@outlook.it>
2024-10-02 20:29:57 +0200

    Aggiunto switch mode per il colore

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,14 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/react.svg";
+import { ColorModeSwitch } from "./ColorModeSwitch";
 
 /* HStack per posizionare orizzontalmente */
 
 export const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent={"space-between"} padding={"10"}>
       <Image src={logo} boxSize={"60px"}></Image>
-      <Text>NavBar</Text>
+      <ColorModeSwitch></ColorModeSwitch>
     </HStack>
   );
 };
