0b64f55148745823cb600aa9df5b8a4807bf8743
FabioFlo <fabio.floris@outlook.it>
2024-10-02 20:00:09 +0200

    Build a NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
+import { NavBar } from "./components/NavBar";
 
 // Simulazione di connessione alla chat del server - Dati
 /* const connect = () => console.log("Connecting");
@@ -161,8 +162,8 @@ function App() {
     <Grid
       templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
     >
-      <GridItem area={"nav"} bg={"coral"}>
-        Nav
+      <GridItem area={"nav"}>
+        <NavBar></NavBar>
       </GridItem>{" "}
       <Show above="lg">
         <GridItem area={"aside"} bg={"gold"}>
